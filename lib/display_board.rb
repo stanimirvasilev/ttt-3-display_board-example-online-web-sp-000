0a3058ceb24e39c8ffab8c557b7fde37b6c60943
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-07-05 17:22:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
  def display_board
- puts "   ","|","   ","|","   "
+ puts "   |   |   "
  
  puts "___________"
  
- puts "   ","|","   ","|","   "
+ puts "   |   |   "
  
  puts  "___________"
  
- puts  "   ","|","   ","|","   "
+ puts "   |   |   "
  end
  
  
